dd1a855b055f70b494ee84a78d189ff1fde2d7ec
Marcin Juszkiewicz <marcin@juszkiewicz.com.pl>
2009-11-26 16:05:23 +0100

    indent

--- hrwplayer.cpp
+++ hrwplayer.cpp
@@ -3,7 +3,7 @@
 
 #include "hrwplayer.h"
 
-QString moduleFileName = "../daydreams2.mod";
+QString moduleFileName;
 
 HrwPlayer::HrwPlayer()
 {
@@ -17,14 +17,14 @@ HrwPlayer::HrwPlayer()
 
     Phonon::createPath(mediaObject, audioOutput);
 
-//    connect(mediaObject, SIGNAL(tick(qint64)), this, SLOT(tick(qint64)));
+    //    connect(mediaObject, SIGNAL(tick(qint64)), this, SLOT(tick(qint64)));
     connect(mediaObject, SIGNAL(stateChanged(Phonon::State,Phonon::State)),
 	    this, SLOT(StateChanged(Phonon::State,Phonon::State)));
-//    connect(metaInformationResolver, SIGNAL(stateChanged(Phonon::State,Phonon::State)),
-//            this, SLOT(metaStateChanged(Phonon::State,Phonon::State)));
-//    connect(mediaObject, SIGNAL(currentSourceChanged(Phonon::MediaSource)),
-//            this, SLOT(sourceChanged(Phonon::MediaSource)));
-//    connect(mediaObject, SIGNAL(aboutToFinish()), this, SLOT(aboutToFinish()));
+    //    connect(metaInformationResolver, SIGNAL(stateChanged(Phonon::State,Phonon::State)),
+    //            this, SLOT(metaStateChanged(Phonon::State,Phonon::State)));
+    //    connect(mediaObject, SIGNAL(currentSourceChanged(Phonon::MediaSource)),
+    //            this, SLOT(sourceChanged(Phonon::MediaSource)));
+    //    connect(mediaObject, SIGNAL(aboutToFinish()), this, SLOT(aboutToFinish()));
 
     connect(LoadButton,  SIGNAL(clicked()), this,        SLOT(OpenFileName()));
     connect(PlayButton,  SIGNAL(clicked()), mediaObject, SLOT(play()));
@@ -51,38 +51,38 @@ void HrwPlayer::OpenFileName()
 
 void HrwPlayer::StateChanged(Phonon::State newState, Phonon::State /* oldState */)
 {
-    switch (newState) {
-        case Phonon::ErrorState:
-            if (mediaObject->errorType() == Phonon::FatalError) {
-                QMessageBox::warning(this, tr("Fatal Error"),
-                mediaObject->errorString());
-            } else {
-                QMessageBox::warning(this, tr("Error"),
-                mediaObject->errorString());
-            }
-            break;
-//![9]
-//![10]
-        case Phonon::PlayingState:
-                PlayButton->setEnabled(false);
-                PauseButton->setEnabled(true);
-                StopButton->setEnabled(true);
-                break;
-        case Phonon::StoppedState:
-                StopButton->setEnabled(false);
-                PlayButton->setEnabled(true);
-                PauseButton->setEnabled(false);
-//                timeLcd->display("00:00");
-                break;
-        case Phonon::PausedState:
-                PauseButton->setEnabled(false);
-                StopButton->setEnabled(true);
-                PlayButton->setEnabled(true);
-                break;
-//![10]
-        case Phonon::BufferingState:
-                break;
-        default:
-            ;
+    switch (newState) 
+    {
+	case Phonon::ErrorState:
+	    if (mediaObject->errorType() == Phonon::FatalError) 
+	    {
+		QMessageBox::warning(this, tr("Fatal Error"),
+			mediaObject->errorString());
+	    }
+	    else
+	    {
+		QMessageBox::warning(this, tr("Error"),
+			mediaObject->errorString());
+	    }
+	    break;
+	case Phonon::PlayingState:
+	    PlayButton->setEnabled(false);
+	    PauseButton->setEnabled(true);
+	    StopButton->setEnabled(true);
+	    break;
+	case Phonon::StoppedState:
+	    StopButton->setEnabled(false);
+	    PlayButton->setEnabled(true);
+	    PauseButton->setEnabled(false);
+	    break;
+	case Phonon::PausedState:
+	    PauseButton->setEnabled(false);
+	    StopButton->setEnabled(true);
+	    PlayButton->setEnabled(true);
+	    break;
+	case Phonon::BufferingState:
+	    break;
+	default:
+	    ;
     }
 }
