cefeb24187ff7d7e04eed711c66660cb4ca35c3f
Michal Schulz <michal.schulz@gmx.de>
2018-10-14 13:36:19 +0200

    number of segments (green,yellow,red) adjustable. Decay done in cpp class. maximum marker added with selectable visibilit

--- vumeter.cpp
+++ vumeter.cpp
@@ -4,10 +4,21 @@
 VUMeter::VUMeter(QQuickItem *parent) : QQuickPaintedItem(parent)
 {
     m_MinValue = 0;
-    m_MaxValue = 100;
-    m_Value = 50;
+    m_MaxValue = 1;
+    m_Value = m_MinValue;
+    m_CurrentValue = m_MinValue;
+    m_CurrentMarker = m_MinValue;
+    m_MarkerVisible = true;
+    m_TotalSegments = 40;
+    m_WarnSegments = 10;
+    m_ErrSegments = 5;
 
+    m_DecayTimer = new QTimer(this);
+
+    connect(m_DecayTimer, SIGNAL(timeout()), this, SLOT(handleSignalDecay()));
     connect(this, SIGNAL(enabledChanged()), this, SLOT(doEnabledChanged()));
+
+    m_DecayTimer->start(20);
 }
 
 VUMeter::~VUMeter()
@@ -28,6 +39,11 @@ void VUMeter::setValue(double v)
         v = m_MaxValue;
 
     m_Value = v;
+    if (v > m_CurrentValue)
+        m_CurrentValue = v;
+
+    if (m_CurrentValue > m_CurrentMarker)
+        m_CurrentMarker = m_CurrentValue;
 
     emit valueChanged();
 
@@ -52,14 +68,29 @@ void VUMeter::setMaximumValue(double v)
     update();
 }
 
+void VUMeter::handleSignalDecay()
+{
+    if (m_CurrentValue >= m_Value + 0.1*(m_MaxValue - m_MinValue) / m_TotalSegments)
+        m_CurrentValue = m_CurrentValue + 0.2 * (m_Value - m_CurrentValue);
+    else
+        m_CurrentValue = m_Value;
+
+    if (m_CurrentMarker >= m_Value + 0.1*(m_MaxValue - m_MinValue) / m_TotalSegments)
+        m_CurrentMarker = m_CurrentMarker + 0.02 * (m_Value - m_CurrentMarker);
+    else
+        m_CurrentMarker = m_Value;
+
+    update();
+}
+
 void VUMeter::paint(QPainter *painter)
 {
     qreal _w = width();
     qreal _h = height();
 
-    qreal _wblock = 7*(_w / (31*7 + 30*2));
-    qreal _wspc = 2*(_w / (31*7 + 30*2));
-    qreal _vstep = (m_MaxValue - m_MinValue) / 31.0;
+    qreal _wblock = 7*(_w / (m_TotalSegments*7 + (m_TotalSegments - 1)*2));
+    qreal _wspc = 2*(_w / (m_TotalSegments*7 + (m_TotalSegments - 1)*2));
+    qreal _vstep = (m_MaxValue - m_MinValue) / (double)m_TotalSegments;
     qreal _v = m_MinValue;
     qreal _x = 0.0;
 
@@ -73,11 +104,13 @@ void VUMeter::paint(QPainter *painter)
     QColor orange(235,195,46);
     QColor red(255,91,56);
 
-    for (int i=0; i < 21; i++)
+    for (int i=0; i < (m_TotalSegments - m_WarnSegments - m_ErrSegments); i++)
     {
         QRectF rect(_x, 0, _wblock, _h);
 
-        if (m_Value > _v)
+        if (m_CurrentValue > _v)
+            painter->fillRect(rect, green);
+        else if (m_MarkerVisible && (m_CurrentMarker > _v && (m_CurrentMarker < (_v + _vstep))))
             painter->fillRect(rect, green);
         else
             painter->fillRect(rect, gray);
@@ -85,11 +118,13 @@ void VUMeter::paint(QPainter *painter)
         _x += _wblock + _wspc;
         _v += _vstep;
     }
-    for (int i=0; i < 5; i++)
+    for (int i=0; i < m_WarnSegments; i++)
     {
         QRectF rect(_x, 0, _wblock, _h);
 
-        if (m_Value > _v)
+        if (m_CurrentValue > _v)
+            painter->fillRect(rect, orange);
+        else if (m_MarkerVisible && (m_CurrentMarker > _v && (m_CurrentMarker < (_v + _vstep))))
             painter->fillRect(rect, orange);
         else
             painter->fillRect(rect, gray);
@@ -97,11 +132,13 @@ void VUMeter::paint(QPainter *painter)
         _x += _wblock + _wspc;
         _v += _vstep;
     }
-    for (int i=0; i < 5; i++)
+    for (int i=0; i < m_ErrSegments; i++)
     {
         QRectF rect(_x, 0, _wblock, _h);
 
-        if (m_Value > _v)
+        if (m_CurrentValue > _v)
+            painter->fillRect(rect, red);
+        else if (m_MarkerVisible && (m_CurrentMarker > _v && (m_CurrentMarker < (_v + _vstep))))
             painter->fillRect(rect, red);
         else
             painter->fillRect(rect, gray);
--- vumeter.h
+++ vumeter.h
@@ -3,6 +3,7 @@
 
 #include <QQuickPaintedItem>
 #include <QQuickItem>
+#include <QTimer>
 
 class VUMeter : public QQuickPaintedItem
 {
@@ -12,6 +13,10 @@ class VUMeter : public QQuickPaintedItem
     Q_PROPERTY(qreal value READ value WRITE setValue NOTIFY valueChanged)
     Q_PROPERTY(qreal minimumValue READ minimumValue WRITE setMinimumValue NOTIFY minimumValueChanged)
     Q_PROPERTY(qreal maximumValue READ maximumValue WRITE setMaximumValue NOTIFY maximumValueChanged)
+    Q_PROPERTY(int totalSegments READ totalSegments WRITE setTotalSegments)
+    Q_PROPERTY(int warningSegments READ warningSegments WRITE setWarningSegments)
+    Q_PROPERTY(int errorSegments READ errorSegments WRITE setErrorSegments)
+    Q_PROPERTY(bool markerVisible READ markerVisible WRITE setMarkerVisible)
 
 public:
     VUMeter(QQuickItem *parent = nullptr);
@@ -23,6 +28,16 @@ public:
     void setMinimumValue(double);
     void setValue(double);
 
+    int totalSegments() { return m_TotalSegments; }
+    int warningSegments() { return m_WarnSegments; }
+    int errorSegments() { return m_ErrSegments; }
+    bool markerVisible() { return m_MarkerVisible; }
+
+    void setTotalSegments(int s) { m_TotalSegments = s; update(); }
+    void setWarningSegments(int s) { m_WarnSegments = s; update(); }
+    void setErrorSegments(int s) { m_ErrSegments = s; update(); }
+    void setMarkerVisible(bool v) { m_MarkerVisible = v; update(); }
+
     void paint(QPainter *painter);
 
 signals:
@@ -32,11 +47,20 @@ signals:
 
 private slots:
     void doEnabledChanged();
+    void handleSignalDecay();
 
 private:
+    int     m_TotalSegments;
+    int     m_WarnSegments;
+    int     m_ErrSegments;
     double  m_MinValue;
     double  m_MaxValue;
     double  m_Value;
+    double  m_CurrentValue;
+    double  m_CurrentMarker;
+    bool    m_MarkerVisible;
+
+    QTimer *m_DecayTimer;
 };
 
 #endif // VUMETER_H
